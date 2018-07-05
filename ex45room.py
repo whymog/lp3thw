6acb06dfe02f8bbab23953a1ebde4f3460071142
Nick Cummings <nickmcummings@gmail.com>
2018-07-05 15:19:39 -0700

    Add method to catch int input failures

--- ex45room.py
+++ ex45room.py
@@ -11,6 +11,25 @@ class Room(object):
         print("You need to subclass it and then implement its enter() method.")
         exit(1)
 
+    def try_int_input(self, prompt):
+        try:
+            int_input = int(input(prompt))
+            return int_input
+        except:
+            return self.try_int_input("Please enter a number.\n> ")
+
+    def actions(self):
+        print("\n1. I decided to look around.")
+        print("2. Time to move on.")
+        choice = input("> ")
+        if choice == 1:
+            self.list_items()
+        elif choice == 2:
+            self.list_exits()
+        else:
+            print("I'm not sure what that meant. Let me try again.")
+            self.actions()
+
     def list_items(self):
         # First, list all items in the room
         print("You see:")
@@ -31,11 +50,11 @@ class Room(object):
                     print("• {}. Take {}".format(counter, item["name"]))
                 print("• 0. Don't take anything")
             
-            choice = input("> ")
-            if int(choice) > 0 and int(choice) < len(items_to_take) - 1:
+            choice = self.try_int_input("> ")
+            if choice > 0 and choice < len(items_to_take) - 1:
                 # Take that item
                 pass
-            elif int(choice) == 0:
+            elif choice == 0:
                 # Go back to room description
                 pass
             else:
